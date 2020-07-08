3952b71ac3cc5046c7aaa3762c21d5cb9cf42dfe
Sunxz007 <977622085@qq.com>
2020-07-08 22:29:30 +0800

    Robbin 负载均衡

--- cloud-consumer-order80/src/main/java/com/sun/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/sun/springcloud/OrderMain80.java
@@ -1,11 +1,18 @@
 package com.sun.springcloud;
 
+import com.sun.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
+/**
+ * @author sun
+ * 启动该微服务时就能去加载我们自定义的Ribbon配置类，从而使配置生效
+ */
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name="CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
